7c6559c0d3ff4fc2aba0d39601849a80b668b301
Veronika Paschenko <veronikapaschenkor29@gmail.com>
2026-03-29 15:00:00 +0300

    рефакторизація: експортує мемоізацію та чергу

--- generators-lib/src/index.js
+++ generators-lib/src/index.js
@@ -1,7 +1,11 @@
 const { randomNumberGenerator } = require('./generators');
 const { timeoutIterator } = require('./timeout-iterator');
+const { createMemoizedFunction } = require('./memoization-core');
+const { BiDirectionalPriorityQueue } = require('./priority-queue');
 
 module.exports = {
     randomNumberGenerator,
-    timeoutIterator
+    timeoutIterator,
+    createMemoizedFunction,
+    BiDirectionalPriorityQueue
 };
