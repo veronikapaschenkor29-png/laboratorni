b7c20120c98e3cd801f0f72a90c1fa710d2682e5
Veronika Paschenko <veronikapaschenkor29@gmail.com>
2026-04-06 10:00:00 +0300

    task-5: асинхронні функції для масивів - основна реалізація

--- generators-lib/src/index.js
+++ generators-lib/src/index.js
@@ -2,10 +2,14 @@ const { randomNumberGenerator } = require('./generators');
 const { timeoutIterator } = require('./timeout-iterator');
 const { createMemoizedFunction } = require('./memoization-core');
 const { BiDirectionalPriorityQueue } = require('./priority-queue');
+const { asyncMap, asyncMapPromise, asyncMapAsync } = require('./async-array');
 
 module.exports = {
     randomNumberGenerator,
     timeoutIterator,
     createMemoizedFunction,
-    BiDirectionalPriorityQueue
+    BiDirectionalPriorityQueue,
+    asyncMap,
+    asyncMapPromise,
+    asyncMapAsync
 };
